9e9c18faf0d954346100655a1a1c3402eae49af6
mpark <mpark@cs.odu.edu>
2018-03-18 16:38:17 -0400

    Removed duplicate lines

--- src/main/java/edu/odu/cs/gold/ParkODUApplication.java
+++ src/main/java/edu/odu/cs/gold/ParkODUApplication.java
@@ -85,7 +85,6 @@ public class ParkODUApplication implements ApplicationContextAware, ApplicationL
         Predicate predicatetemp = Predicates.equal("id",isEmpty);
         userRepository.deleteByPredicate(predicatetemp);
 
-
         System.out.println("# of Garages loaded from Mongo: " + garageRepository.findAll().size());
         System.out.println("# of Floors loaded from Mongo: " + floorRepository.findAll().size());
         System.out.println("# of ParkingSpaces loaded from Mongo: " + parkingSpaceRepository.findAll().size());
@@ -94,8 +93,6 @@ public class ParkODUApplication implements ApplicationContextAware, ApplicationL
         System.out.println("# of TravelDistanceDurations loaded from Mongo: " + travelDistanceDurationRepository.findAll().size());
         System.out.println("# of permit types loaded from Mongo: " + permitTypeRepository.findAll().size());
         System.out.println("# of space types loaded from Mongo: " + spaceTypeRepository.findAll().size());
-        System.out.println("# of PermitTypeRepository loaded from Mongo: " + permitTypeRepository.findAll().size());
-        System.out.println("# of SpaceTypeRepository loaded from Mongo: " + spaceTypeRepository.findAll().size());
 
         if (false) {
                     /*
