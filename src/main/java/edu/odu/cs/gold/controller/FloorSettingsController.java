6e6b6eb533d5cf2e923dfbb15d725719b067cd31
mpark <mpark@cs.odu.edu>
2018-03-22 00:19:53 -0400

    FloorSettingsController.java was fully commented and all floor configuration functionality was tested.

--- src/main/java/edu/odu/cs/gold/controller/FloorSettingsController.java
+++ src/main/java/edu/odu/cs/gold/controller/FloorSettingsController.java
@@ -5,14 +5,12 @@ import com.hazelcast.query.Predicates;
 import edu.odu.cs.gold.model.Floor;
 import edu.odu.cs.gold.model.Garage;
 import edu.odu.cs.gold.model.ParkingSpace;
-import edu.odu.cs.gold.model.SpaceType;
 import edu.odu.cs.gold.repository.*;
 import edu.odu.cs.gold.service.GarageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
@@ -54,10 +52,11 @@ public class FloorSettingsController {
      * This method returns "settings/floor/index" with a collection of all garage objects
      * added to the model.
      *
-     * Usage in settings/floor/index.html to access the collection of all garage
-     * ${garage}
-
-     * @param model Model
+     * @param successMessage String
+     * @param infoMessage String
+     * @param warningMessage String
+     * @param dangerMessage String
+     * @param model String
      * @return String "settings/floor/index"
      */
     @GetMapping({"","/","/index"})
@@ -80,31 +79,96 @@ public class FloorSettingsController {
         return "settings/floor/index";
     }
 
+    /**
+     * This method returns "settings/floor/garage" with a Garage object and
+     * a collection of Floor objects found in the garage.
+     *
+     * @param garageKey String
+     * @param successMessage String
+     * @param infoMessage String
+     * @param warningMessage String
+     * @param dangerMessage String
+     * @param model Model
+     * @return String "settings/floor/garage"
+     */
+    @GetMapping("garage/{garageKey}")
+    public String garage(@PathVariable("garageKey") String garageKey,
+                         @RequestParam(value = "successMessage", required = false) String successMessage,
+                         @RequestParam(value = "infoMessage", required = false) String infoMessage,
+                         @RequestParam(value = "warningMessage", required = false) String warningMessage,
+                         @RequestParam(value = "dangerMessage", required = false) String dangerMessage,
+                         Model model) {
+
+        Garage garage = garageRepository.findByKey(garageKey);
+
+        Predicate predicate = Predicates.and(
+                Predicates.equal("garageKey", garage.getGarageKey())
+        );
+
+        List<Floor> floors = new ArrayList<>(floorRepository.findByPredicate(predicate));
+        floors.sort(Comparator.comparing(Floor::getNumber));
+
+        model.addAttribute("garage", garage);
+        model.addAttribute("floors", floors);
+
+        // Alerts
+        if (successMessage != null) { model.addAttribute("successMessage", successMessage); }
+        if (infoMessage != null) { model.addAttribute("infoMessage", infoMessage); }
+        if (warningMessage != null) { model.addAttribute("warningMessage", warningMessage); }
+        if (dangerMessage != null) { model.addAttribute("dangerMessage", dangerMessage); }
+
+        return "settings/floor/garage";
+    }
+
+    /**
+     * Method that returns the settings/floor/create.html template page with model attributes
+     * @param successMessage String
+     * @param infoMessage String
+     * @param warningMessage String
+     * @param dangerMessage String
+     * @param garageKey String
+     * @param model Model
+     * @return String "settings/floor/create"
+     */
     @GetMapping("/create/{garageKey}")
-    public String create(@PathVariable("garageKey") String garageKey, Model model) {
+    public String create(@RequestParam(value = "successMessage", required = false) String successMessage,
+                         @RequestParam(value = "infoMessage", required = false) String infoMessage,
+                         @RequestParam(value = "warningMessage", required = false) String warningMessage,
+                         @RequestParam(value = "dangerMessage", required = false) String dangerMessage,
+                         @PathVariable("garageKey") String garageKey, Model model) {
         Garage garage = garageRepository.findByKey(garageKey);
         Floor floor = new Floor();
         floor.setGarageKey(garage.getGarageKey());
         model.addAttribute("floor", floor);
         model.addAttribute("garage", garage);
+
+        // Alerts
+        if (successMessage != null) { model.addAttribute("successMessage", successMessage); }
+        if (infoMessage != null) { model.addAttribute("infoMessage", infoMessage); }
+        if (warningMessage != null) { model.addAttribute("warningMessage", warningMessage); }
+        if (dangerMessage != null) { model.addAttribute("dangerMessage", dangerMessage); }
+
         return "settings/floor/create";
     }
 
+    /**
+     * Method that creates a new floor
+     * @param floor Floor
+     * @param redirectAttributes RedirectAttributes
+     * @return String redirection to previous page
+     */
     @PostMapping("/create")
     public String create(Floor floor,
-                         RedirectAttributes redirectAttributes,
-                         Model model) {
+                         RedirectAttributes redirectAttributes) {
+
         boolean isSuccessful = false;
         boolean isDuplicate = false;
 
-        System.err.println(floor.toString());
-
         if(floor.getGarageKey() == null || floor.getGarageKey().isEmpty()) {
-            List<Garage> garages = new ArrayList<>(garageRepository.findAll());
-            garages.sort(Comparator.comparing(Garage::getName));
-            model.addAttribute("dangerMessage", "The garage key cannot be null or empty.");
-            model.addAttribute("garages", garages);
-            return "settings/floor/index";
+            redirectAttributes.addAttribute(
+                    "dangerMessage",
+                    "The garage key cannot be null or empty.");
+            return "redirect:/settings/floor/index";
         }
 
         Predicate predicate = Predicates.and(
@@ -137,19 +201,39 @@ public class FloorSettingsController {
             Garage garage = garageRepository.findByKey(floor.getGarageKey());
             redirectAttributes.addAttribute(
                     "successMessage",
-                    "The floor was successfully created in garage " + garage.getName());
+                    "The floor, " +
+                            floor.getNumber() +
+                            ", was successfully created in garage " +
+                            garage.getName());
+
+            return "redirect:/settings/floor/garage/" + floor.getGarageKey();
         }
 
         if(isDuplicate) {
             Garage garage = garageRepository.findByKey(floor.getGarageKey());
             redirectAttributes.addAttribute(
                     "dangerMessage",
-                    "The specified floor already exists in " + garage.getName());
+                    "The floor " +
+                                floor.getNumber() +
+                                " already exists in " +
+                                garage.getName());
+
+            return "redirect:/settings/floor/create/" + floor.getGarageKey();
         }
 
         return "redirect:/settings/floor/garage/" + floor.getGarageKey();
     }
 
+    /**
+     * Method that returns the "settings/floor/edit" template page
+     * @param floorKey String
+     * @param successMessage String
+     * @param infoMessage String
+     * @param warningMessage String
+     * @param dangerMessage String
+     * @param model Model
+     * @return String "settings/floor/edit"
+     */
     @GetMapping("/edit/{floorKey}")
     public String edit(@PathVariable("floorKey") String floorKey,
                        @RequestParam(value = "successMessage", required = false) String successMessage,
@@ -170,9 +254,14 @@ public class FloorSettingsController {
         return "settings/floor/edit";
     }
 
+    /**
+     * Method to edit the current floor attributes
+     * @param floor Floor
+     * @param redirectAttributes RedirectAttributes
+     * @return String redirection to previous page
+     */
     @PostMapping("/edit")
     public String edit(Floor floor,
-                       Model model,
                        RedirectAttributes redirectAttributes) {
         boolean isSuccessful = false;
         boolean isDuplicate = false;
@@ -219,13 +308,12 @@ public class FloorSettingsController {
                             " was successfully updated.");
         }
         else if (isDuplicate) {
-            model.addAttribute(
+            redirectAttributes.addAttribute(
                     "dangerMessage",
                     "A floor with the number " +
                             floor.getNumber() +
                             " already exists.");
-            model.addAttribute("floor", floor);
-            return "settings/floor/edit";
+            return "redirect:/settings/floor/edit/" + floor.getFloorKey();
         }
         else {
             redirectAttributes.addAttribute(
@@ -237,66 +325,29 @@ public class FloorSettingsController {
     }
 
     /**
-     * This method returns "settings/floor/garage" with a Garage object and
-     * a collection of Floor objects found in the garage.
-     *
-     * Uage in settings/floor/garage.html to access the collection of floors and the garage
-     * ${garage}
-     * ${floors}
-     *
-     * @param garageKey String unique identifier of the garage being queried
-     * @param model Model
-     * @return String "settings/floor/garage"
+     * Method to remove a floor and delete any spaces on the floor
+     * @param floorKey Strong
+     * @param redirectAttributes RedirectAttributes
+     * @return String redirection to previous page
      */
-    @GetMapping("garage/{garageKey}")
-    public String garage(@PathVariable("garageKey") String garageKey,
-                         @RequestParam(value = "successMessage", required = false) String successMessage,
-                         @RequestParam(value = "infoMessage", required = false) String infoMessage,
-                         @RequestParam(value = "warningMessage", required = false) String warningMessage,
-                         @RequestParam(value = "dangerMessage", required = false) String dangerMessage,
-                        Model model) {
-
-        Garage garage = garageRepository.findByKey(garageKey);
-
-        Predicate predicate = Predicates.and(
-                Predicates.equal("garageKey", garage.getGarageKey())
-        );
-
-        List<Floor> floors = new ArrayList<>(floorRepository.findByPredicate(predicate));
-        floors.sort(Comparator.comparing(Floor::getNumber));
-
-        model.addAttribute("garage", garage);
-        model.addAttribute("floors", floors);
-
-        // Alerts
-        if (successMessage != null) { model.addAttribute("successMessage", successMessage); }
-        if (infoMessage != null) { model.addAttribute("infoMessage", infoMessage); }
-        if (warningMessage != null) { model.addAttribute("warningMessage", warningMessage); }
-        if (dangerMessage != null) { model.addAttribute("dangerMessage", dangerMessage); }
-
-        return "settings/floor/garage";
-    }
-
     @PostMapping("/delete")
     public String delete(@RequestParam("floorKey") String floorKey,
-                         Model model) {
+                         RedirectAttributes redirectAttributes) {
 
         if(floorKey == null || floorKey.isEmpty()) {
-            List<Garage> garages = new ArrayList<>(garageRepository.findAll());
-            garages.sort(Comparator.comparing(Garage::getName));
-            model.addAttribute("dangerMessage", "The floor key cannot be null or empty.");
-            model.addAttribute("garages", garages);
-            return "settings/floor/index";
+            redirectAttributes.addAttribute(
+                    "dangerMessage",
+                    "The floor key cannot be null or empty.");
+            return "redirect:/settings/floor/index";
         }
 
         Floor existingFloor = floorRepository.findByKey(floorKey);
 
         if(existingFloor == null) {
-            List<Garage> garages = new ArrayList<>(garageRepository.findAll());
-            garages.sort(Comparator.comparing(Garage::getName));
-            model.addAttribute("dangerMessage", "The floor key was not found.");
-            model.addAttribute("garages", garages);
-            return "settings/floor/index";
+            redirectAttributes.addAttribute(
+                    "dangerMessage",
+                    "The floor key was not found.");
+            return "redirect:/settings/floor/index";
         }
         else {
             Predicate spacePredicate = Predicates.and(
@@ -309,13 +360,11 @@ public class FloorSettingsController {
             floorRepository.delete(floorKey);
             garageService.refresh(garageKey);
 
-            Predicate floorPredicate = Predicates.equal("garageKey", garageKey);
-            List<Floor> floors = floorRepository.findByPredicate(floorPredicate);
-            floors.sort(Comparator.comparing(Floor::getNumber));
-            Garage garage = garageRepository.findByKey(garageKey);
-            model.addAttribute("floors", floors);
-            model.addAttribute("successMessage", "The floor key was deleted.");
-            return "settings/floor/garage";
+            redirectAttributes.addAttribute(
+                    "successMessage",
+                    "The floor was successfully deleted.");
+
+            return "redirect:/settings/floor/garage/" + garageKey;
         }
     }
 }
