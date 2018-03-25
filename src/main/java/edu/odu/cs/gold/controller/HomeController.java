6be484b4df8bfa3b389b252dc21f84d23c5c028a
gsilverio <gsilverio@longviewinc.com>
2018-03-25 04:41:56 -0400

    mapped the Current Garage Availability chart in the main landing page to actual Garage availability

--- src/main/java/edu/odu/cs/gold/controller/HomeController.java
+++ src/main/java/edu/odu/cs/gold/controller/HomeController.java
@@ -1,5 +1,11 @@
 package edu.odu.cs.gold.controller;
 
+import edu.odu.cs.gold.model.Garage;
+import edu.odu.cs.gold.repository.GarageRepository;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
-public class HomeController {
+public class HomeController implements ApplicationContextAware {
+
+    @Autowired
+    private GarageRepository garageRepository;
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
     @GetMapping({"","/","/index"})
-    public String index() {
+    public String index(Model model) {
+        List<Garage> garages = new ArrayList<>(garageRepository.findAll());
+        garages.sort(Comparator.comparing(Garage::getName));
+        StringBuilder currentAvailabilityDataString = new StringBuilder();
+        for (Garage garage : garages) {
+            currentAvailabilityDataString.append(garage.getAvailableSpaces() + ",");
+        }
+        model.addAttribute("currentAvailabilityDataString", currentAvailabilityDataString.toString());
         return "home/index";
     }
 
@@ -37,4 +63,5 @@ public class HomeController {
     public String logout() {
         return "redirect:/";
     }
+
 }
