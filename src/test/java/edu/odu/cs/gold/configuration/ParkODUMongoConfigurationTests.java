4f0547c4a9e37b94577fda60dd27d697e7eb6f1c
gsilverio <gsilverio@longviewinc.com>
2018-03-27 21:32:25 -0400

    added base junit test classes for configuration package

---
+++ src/test/java/edu/odu/cs/gold/configuration/ParkODUMongoConfigurationTests.java
@@ -0,0 +1,20 @@
+package edu.odu.cs.gold.configuration;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParkODUMongoConfigurationTests {
+
+    private ParkODUMongoConfiguration parkODUMongoConfiguration;
+
+    @Before
+    public void setup() {
+        parkODUMongoConfiguration = new ParkODUMongoConfiguration();
+    }
+
+    @Test
+    public void constructor() {
+
+    }
+
+}
