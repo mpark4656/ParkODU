8a0fa84150531f09052d79302337ec064c93b307
mpark <mpark@cs.odu.edu>
2018-03-29 18:06:08 -0400

    Added Scheduled tasks for generating FloorStatistic objects

--- src/main/java/edu/odu/cs/gold/model/FloorStatistic.java
+++ src/main/java/edu/odu/cs/gold/model/FloorStatistic.java
@@ -2,6 +2,7 @@ package edu.odu.cs.gold.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 public class FloorStatistic implements Serializable {
 
@@ -9,6 +10,10 @@ public class FloorStatistic implements Serializable {
     private String floorKey;
     private Double capacity;
     private Date timestamp;
+
+    public FloorStatistic() {
+        floorStatisticKey = UUID.randomUUID().toString();
+    }
 
     public String getFloorStatisticKey() {
         return floorStatisticKey;
