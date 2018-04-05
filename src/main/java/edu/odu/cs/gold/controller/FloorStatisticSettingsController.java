f541d733a2a67d8355d9aea0a910c51aea8930d1
asheikh <ashei005@odu.edu>
2018-04-04 22:29:11 -0400

    Committing my progress for tonight

---
+++ src/main/java/edu/odu/cs/gold/controller/FloorStatisticSettingsController.java
@@ -0,0 +1,78 @@
+package edu.odu.cs.gold.controller;
+
+
+import edu.odu.cs.gold.model.Floor;
+import edu.odu.cs.gold.model.Garage;
+import edu.odu.cs.gold.repository.FloorRepository;
+import edu.odu.cs.gold.repository.FloorStatisticRepository;
+import edu.odu.cs.gold.repository.GarageRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
+@Controller
+@RequestMapping ("/settings/floorstatistics")
+
+
+public class FloorStatisticSettingsController {
+
+    private GarageRepository garageRepository;
+    private FloorRepository floorRepository;
+    private FloorStatisticRepository floorStatisticRepository;
+
+    public FloorStatisticSettingsController(GarageRepository garageRepository,
+                                            FloorRepository floorRepository,
+                                            FloorStatisticRepository floorStatisticRepository)
+    {
+        this.garageRepository = garageRepository;
+        this.floorRepository = floorRepository;
+        this.floorStatisticRepository =floorStatisticRepository;
+
+    }
+
+    @GetMapping({"", "/", "/index"})
+    public String index(@RequestParam(value = "successMessage", required = false) String successMessage,
+                        @RequestParam(value = "infoMessage", required = false) String infoMessage,
+                        @RequestParam(value = "warningMessage", required = false) String warningMessage,
+                        @RequestParam(value = "dangerMessage", required = false) String dangerMessage,
+                        Model model) {
+
+        List<Garage> garages = new ArrayList<>(garageRepository.findAll());
+        garages.sort(Comparator.comparing(Garage::getName));
+
+        List<Floor> floors = new ArrayList<>(floorRepository.findAll());
+        floors.sort(Comparator.comparing(Floor::getNumber));
+
+
+        model.addAttribute("garages", garages);
+        model.addAttribute("floors", floors);
+
+        // Alerts
+        if (successMessage != null) {
+            model.addAttribute("successMessage", successMessage);
+        }
+        if (infoMessage != null) {
+            model.addAttribute("infoMessage", infoMessage);
+        }
+        if (warningMessage != null) {
+            model.addAttribute("warningMessage", warningMessage);
+        }
+        if (dangerMessage != null) {
+            model.addAttribute("dangerMessage", dangerMessage);
+        }
+
+
+        return "settings/floorstatistics/index";
+
+
+    }
+}
+
+
