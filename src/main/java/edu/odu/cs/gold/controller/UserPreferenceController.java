dd2566347b500e794d07d31801ff65de1eefc646
mpark <mpark@cs.odu.edu>
2018-04-05 21:43:24 -0400

    Finished implementing User Preference Page (Now called Settings in the dropdown menu).
    Users can now look at their profile and see their name, username, and email, and have the option of editing them
    
    Users can now reset their own password if they wish to.
    
    Users can save default Starting Address, Permit Types, Space types, and Destination Building and these settings will be automatically set when user navigates to the Search page

--- src/main/java/edu/odu/cs/gold/controller/UserPreferenceController.java
+++ src/main/java/edu/odu/cs/gold/controller/UserPreferenceController.java
@@ -2,9 +2,11 @@ package edu.odu.cs.gold.controller;
 
 import com.hazelcast.query.Predicate;
 import com.hazelcast.query.Predicates;
+import edu.odu.cs.gold.model.Building;
 import edu.odu.cs.gold.model.PermitType;
 import edu.odu.cs.gold.model.SpaceType;
 import edu.odu.cs.gold.model.User;
+import edu.odu.cs.gold.repository.BuildingRepository;
 import edu.odu.cs.gold.repository.PermitTypeRepository;
 import edu.odu.cs.gold.repository.SpaceTypeRepository;
 import edu.odu.cs.gold.repository.UserRepository;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -28,18 +32,25 @@ public class UserPreferenceController {
     private UserRepository userRepository;
     private PermitTypeRepository permitTypeRepository;
     private SpaceTypeRepository spaceTypeRepository;
+    private BuildingRepository buildingRepository;
 
 
     public UserPreferenceController(UserRepository userRepository,
                                     PermitTypeRepository permitTypeRepository,
-                                    SpaceTypeRepository spaceTypeRepository) {
+                                    SpaceTypeRepository spaceTypeRepository,
+                                    BuildingRepository buildingRepository) {
         this.userRepository = userRepository;
         this.permitTypeRepository = permitTypeRepository;
         this.spaceTypeRepository = spaceTypeRepository;
+        this.buildingRepository = buildingRepository;
     }
 
     @GetMapping({"", "/", "/index"})
-    public String index(Model model) {
+    public String index(@RequestParam(value = "successMessage", required = false) String successMessage,
+                        @RequestParam(value = "infoMessage", required = false) String infoMessage,
+                        @RequestParam(value = "warningMessage", required = false) String warningMessage,
+                        @RequestParam(value = "dangerMessage", required = false) String dangerMessage,
+                        Model model) {
         try {
             AuthenticatedUser authenticatedUser =
                     (AuthenticatedUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -54,42 +65,145 @@ public class UserPreferenceController {
                 User user = users.get(0);
                 List<PermitType> permitTypes = new ArrayList<>(permitTypeRepository.findAll());
                 List<SpaceType> spaceTypes = new ArrayList<> (spaceTypeRepository.findAll());
+                List<Building> buildings = new ArrayList<> (buildingRepository.findAll());
 
                 permitTypes.sort(Comparator.comparing(PermitType::getName));
                 spaceTypes.sort(Comparator.comparing(SpaceType::getName));
+                buildings.sort(Comparator.comparing(Building::getName));
 
                 model.addAttribute("user", user);
                 model.addAttribute("permitTypes", permitTypes);
                 model.addAttribute("spaceTypes", spaceTypes);
+                model.addAttribute("buildings", buildings);
             }
         } catch(Exception e) {
             e.printStackTrace();
         }
 
+        // Alerts
+        if (successMessage != null) { model.addAttribute("successMessage", successMessage); }
+        if (infoMessage != null) { model.addAttribute("infoMessage", infoMessage); }
+        if (warningMessage != null) { model.addAttribute("warningMessage", warningMessage); }
+        if (dangerMessage != null) { model.addAttribute("dangerMessage", dangerMessage); }
+
         return "user_preference/index";
     }
 
     @PostMapping("/edit")
-    public String edit(@RequestParam("userKey") String userKey,
-                       @RequestParam(name = "preferredPermitTypes", required = false) List<String> preferredPermitTypes,
-                       @RequestParam(name = "preferredSpaceTypes", required = false) List<String> preferredSpaceTypes) {
+    public String edit(User user,
+                       RedirectAttributes redirectAttributes) {
 
-        User user = userRepository.findByKey(userKey);
+        boolean duplicateEmail = false;
+        boolean duplicateUsername = false;
+        boolean success = false;
 
-        if(preferredPermitTypes == null) {
-            user.setPreferredPermitTypes(new HashSet<>());
+        Predicate emailPredicate = Predicates.and(
+                Predicates.notEqual("userKey", user.getUserKey()),
+                Predicates.equal("email", user.getEmail())
+        );
+
+        Predicate userNamePredicate = Predicates.and(
+                Predicates.notEqual("userKey", user.getUserKey()),
+                Predicates.equal("username", user.getUsername())
+        );
+
+
+        int emailPredicateCount = userRepository.countByPredicate(emailPredicate);
+        int userNamePredicateCount = userRepository.countByPredicate(userNamePredicate);
+        if(emailPredicateCount != 0) {
+            duplicateEmail = true;
+        } else if(userNamePredicateCount != 0) {
+            duplicateUsername = true;
         } else {
-            user.setPreferredPermitTypes(new HashSet<>(preferredPermitTypes));
+            User existingUser = userRepository.findByKey(user.getUserKey());
+
+            existingUser.setFirstName(user.getFirstName());
+            existingUser.setLastName(user.getLastName());
+            existingUser.setUsername(user.getUsername());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPreferredStartingAddress(user.getPreferredStartingAddress());
+            existingUser.setPreferredDestinationBuilding(user.getPreferredDestinationBuilding());
+            existingUser.setPreferredPermitTypes(user.getPreferredPermitTypes());
+            existingUser.setPreferredSpaceTypes(user.getPreferredSpaceTypes());
+
+            /*
+            if(preferredPermitTypes == null) {
+                existingUser.setPreferredPermitTypes(new HashSet<>());
+            } else {
+                existingUser.setPreferredPermitTypes(new HashSet<>(preferredPermitTypes));
+            }
+
+            if(preferredSpaceTypes == null) {
+                existingUser.setPreferredSpaceTypes(new HashSet<> ());
+            } else {
+                existingUser.setPreferredSpaceTypes(new HashSet<>(preferredSpaceTypes));
+            }
+            */
+
+            userRepository.save(existingUser);
+            success = true;
         }
 
-        if(preferredSpaceTypes == null) {
-            user.setPreferredSpaceTypes(new HashSet<> ());
-        } else {
-            user.setPreferredSpaceTypes(new HashSet<>(preferredSpaceTypes));
+        if(success) {
+            redirectAttributes.addAttribute(
+                    "successMessage",
+                    "Your information has been successfully updated."
+            );
         }
 
-        userRepository.save(user);
+        if(duplicateEmail) {
+            redirectAttributes.addAttribute(
+                    "dangerMessage",
+                    "That e-mail address already exists."
+            );
+        }
 
-        return "redirect:/";
+        if(duplicateUsername) {
+            redirectAttributes.addAttribute(
+                    "dangerMessage",
+                    "That username already exists."
+            );
+        }
+
+        return "redirect:/user_preference/index";
+    }
+
+    @PostMapping("/reset_password")
+    public String reset_password(@RequestParam("userKey") String userKey,
+                                 @RequestParam("password") String password,
+                                 @RequestParam("passwordAgain") String passwordAgain,
+                                 RedirectAttributes redirectAttributes) {
+
+        boolean isSuccessful = false;
+        boolean passwordMismatch = false;
+        String username = null;
+
+        try {
+            if (!password.equals(passwordAgain)) {
+                passwordMismatch = true;
+            }
+            else {
+                User existingUser = userRepository.findByKey(userKey);
+                existingUser.setPassword(password);
+                userRepository.save(existingUser);
+                isSuccessful = true;
+                username = existingUser.getUsername();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Alerts
+        if (isSuccessful && username != null) {
+            redirectAttributes.addAttribute("successMessage", "The password for " + username + " was successfully changed.");
+        }
+        else if (passwordMismatch) {
+            redirectAttributes.addAttribute("dangerMessage", "Failed to change the password of a User due to a password mismatch.");
+        }
+        else {
+            redirectAttributes.addAttribute("dangerMessage", "An error occurred when attempting to change a User's password.");
+        }
+        return "redirect:/user_preference/index";
     }
 }
